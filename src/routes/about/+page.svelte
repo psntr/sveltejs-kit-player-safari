eb12de59a647859d8afc83ce39a18a4b0c096ca2
psntr <psn.trieu@gmail.com>
2026-01-05 21:53:12 +0900

    Update +page.svelte HTML escaping

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -10,7 +10,7 @@
 
 <div class="text-column">
 	<h1>About this app</h1>
-	<p>Demonstrates an issue where `<media-theme>` controls work on full page load but break on client-side navigation on Safari Desktop.
+	<p>Demonstrates an issue where <code>&lt;media-theme&gt;</code> controls work on full page load but break on client-side navigation on Safari Desktop when other HTML is present than the player.</p>
 </p>
 	<Player />
 </div>
