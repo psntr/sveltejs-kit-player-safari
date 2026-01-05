1fd80a9b39deb0976ca37bdbecf07763c4563552
psntr <psn.trieu@gmail.com>
2026-01-05 22:05:44 +0900

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	import { resolve } from '$app/paths';
+ 	import 'hls-video-element';
+  	import 'player.style/sutro';
 	import Player from '../Player.svelte';
 </script>
 
@@ -12,5 +14,16 @@
 	<h1>About this app</h1>
 	<p>Demonstrates an issue where <code>&lt;media-theme&gt;</code> controls work on client-side navigation on all browsers except on Safari Desktop when other HTML is present than the player component.</p>
 	<p><strong>Also note:</strong> <code>&lt;media-theme&gt;</code> do not init on full page load </p>
-	<Player />
+	<div style="border: red 1px solid"><Player /></div>
+	<p>Without component and using Packaged Method</p>
+	<div style="border: blue 1px solid">
+		<media-theme-sutro style="width: 100%;">
+		  <hls-video
+		    slot="media"
+		    src="https://stream.mux.com/fXNzVtmtWuyz00xnSrJg4OJH6PyNo6D02UzmgeKGkP5YQ.m3u8"
+		    playsinline
+		    crossorigin="anonymous"
+		  ></hls-video>
+		</media-theme-sutro>
+	</div>
 </div>
