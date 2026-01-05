fc4a84c537918095312a00d4f78ad5da8c0c40ac
psntr <psn.trieu@gmail.com>
2026-01-05 21:54:28 +0900

    Update +page.svelte fix typo

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -10,7 +10,5 @@
 
 <div class="text-column">
 	<h1>About this app</h1>
-	<p>Demonstrates an issue where <code>&lt;media-theme&gt;</code> controls work on full page load but break on client-side navigation on Safari Desktop when other HTML is present than the player.</p>
-</p>
-	<Player />
+	<p>Demonstrates an issue where <code>&lt;media-theme&gt;</code> controls work on full page load but break on client-side navigation on Safari Desktop when other HTML is present than the player.</p>	<Player />
 </div>
